68d231b85aa5ce92f9289f33d7533c2931c3bd47
Craig Eaton <craigea92@gmail.com>
2022-11-11 15:06:00 +0000

    Setup new project and created basic components

---
+++ src/components/Feed.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+const Feed = () => {
+  return (
+    <Stack sx={}
+  )
+}
+
+export default Feed
